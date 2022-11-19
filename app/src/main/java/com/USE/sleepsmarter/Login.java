86656a65ad16d0da1185eef00654b3462d1e4783
clapathiotis <43289218+clapathiotis@users.noreply.github.com>
2022-11-19 18:59:10 +0100

    Now user once logged in they stay logged in

--- app/src/main/java/com/USE/sleepsmarter/Login.java
+++ app/src/main/java/com/USE/sleepsmarter/Login.java
@@ -3,7 +3,9 @@ package com.USE.sleepsmarter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +25,9 @@ import com.google.firebase.database.ValueEventListener;
 public class Login extends AppCompatActivity {
 
     DatabaseReference dbref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://sleepsmarter-6f213-default-rtdb.firebaseio.com/");
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String signed_in = "signed_in" ;
+    private boolean logged;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,63 +37,70 @@ public class Login extends AppCompatActivity {
         final EditText password = findViewById(R.id.passwordEtlogin);
         final Button loginbtn = findViewById(R.id.loginBtn);
         final TextView noaccount = findViewById(R.id.noAccountTy);
+        SharedPreferences shpref = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        logged = shpref.getBoolean(signed_in, false);
+        if (logged) {
+            Log.d("logged", "val" + logged);
+            startActivity(new Intent(Login.this, MainActivity.class));
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // User is signed in
-            Intent i = new Intent(Login.this, MainActivity.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(i);
-        } else {
-            loginbtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    final String phonetxt = phone.getText().toString();
-                    final String passwordtxt = password.getText().toString();
+        }
 
-                    if (phonetxt.isEmpty() || passwordtxt.isEmpty()) {
-                        Toast.makeText(Login.this, "Please enter your name and password,",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        dbref.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                                if(snapshot.hasChild(phonetxt)) {
-                                    final String getpassword = snapshot.child(phonetxt).child("Password").getValue(String.class);
-                                    if(getpassword.equals(passwordtxt)) {
-                                        Toast.makeText(Login.this, "Successful Login", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(Login.this, MainActivity.class));
-                                        finish();
-                                    }
-                                    else {
-                                        Toast.makeText(Login.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
-                                    }
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String phonetxt = phone.getText().toString();
+                final String passwordtxt = password.getText().toString();
+
+                if (phonetxt.isEmpty() || passwordtxt.isEmpty()) {
+                    Toast.makeText(Login.this, "Please enter your name and password,",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    dbref.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(phonetxt)) {
+                                final String getpassword = snapshot.child(phonetxt).child("Password").getValue(String.class);
+                                if(getpassword.equals(passwordtxt)) {
+                                    Toast.makeText(Login.this, "Successful Login", Toast.LENGTH_SHORT).show();
+                                    SharedPreferences shpref = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = shpref.edit();
+                                    editor.putBoolean(signed_in, true).apply();
+                                    boolean val = shpref.getBoolean(signed_in, true);
+                                    Log.d("logged1", "Value " + val );
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
                                 }
                                 else {
                                     Toast.makeText(Login.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
                                 }
-
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
                             }
 
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
+                        }
 
-                            }
-                        });
-                    }
-                }
-            });
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
 
-            noaccount.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(Login.this, Signup_java.class));
+                        }
+                    });
                 }
-            });
-        }
-        }
+            }
+        });
+
+        noaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Signup_java.class));
+            }
+        });
+
+    }
 
 
 }
